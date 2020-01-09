dfa6105bd239fceb254c8c9c4243b36b3d86504b
rgrier104 <rgrier104@gmail.com>
2020-01-09 08:59:24 -1000

    migrate and seed database

--- traveller_backend/db/seeds.rb
+++ traveller_backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+japan = Trip.create(name: 'Japan', status: 'completed')
+sa = Trip.create(name: 'South Africa', status: 'completed')
+alb = Trip.create(name: 'Albania', status: 'completed')
+scot = Trip.create(name: 'Scotland', status: 'upcoming')
+nv = Trip.create(name: 'Napa Valley', status: 'upcoming')
+nz = Trip.create(name: 'New Zealand', status: 'bucketlist')
+banff = Trip.create(name: 'Banff', status: 'bucketlist')
+
+japan.recommendations.create(title: 'The Gate Hotel - Tokyo', description: 'https://www.gate-hotel.jp/en/asakusa-kaminarimon/')
+japan.recommendations.create(title: 'Fushimi Inari Taisha', description: 'Hour outside of Tokyo, go early')
+nv.recommendations.create(title: 'Bouchaine Vineyards')
