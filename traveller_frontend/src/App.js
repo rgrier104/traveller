eca3230ed2aacc4ab9b68664be302bf6fd069836
rgrier104 <rgrier104@gmail.com>
2020-01-09 11:28:29 -1000

    test fetch request to api

--- traveller_frontend/src/App.js
+++ traveller_frontend/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:3000/api/v1/trips')
+      .then(resp => resp.json())
+      .then(json => console.log(json))
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Traveller
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
