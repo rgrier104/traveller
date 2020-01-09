68e8ae10920e8aa242765a90943742234fdb332b
rgrier104 <rgrier104@gmail.com>
2020-01-08 16:38:35 -1000

    add api and v1 folder structure to controllers and update route namespacing

--- traveller_backend/config/routes.rb
+++ traveller_backend/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :recommendations
-  resources :trips
+
+  namespace :api do
+    namespace :v1 do
+      resources :recommendations
+      resources :trips
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
