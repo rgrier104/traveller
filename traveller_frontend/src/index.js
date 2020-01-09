7afc5e41e8c01574e88c387f2982e156dfc0359f
rgrier104 <rgrier104@gmail.com>
2020-01-09 11:35:33 -1000

    import packages to index.js to connect redux

--- traveller_frontend/src/index.js
+++ traveller_frontend/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
