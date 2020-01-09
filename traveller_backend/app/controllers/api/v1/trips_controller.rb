24591364a443418043343f7b0498b492a8ef127b
rgrier104 <rgrier104@gmail.com>
2020-01-09 09:32:17 -1000

    add trip index controller method and strong params

--- traveller_backend/app/controllers/api/v1/trips_controller.rb
+++ traveller_backend/app/controllers/api/v1/trips_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::TripsController < ApplicationController
+
+    def index
+        @trips = Trip.all
+        render json: @trips
+    end
+
+    private
+
+    def trip_params
+        params.require(:trip).permit(:name, :status)
+    end
+
 end
