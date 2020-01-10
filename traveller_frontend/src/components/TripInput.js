642fe74d30b256c767ee07a01047137384bff981
rgrier104 <rgrier104@gmail.com>
2020-01-10 13:45:22 -1000

    add handle onchange function to update state when form changes

--- traveller_frontend/src/components/TripInput.js
+++ traveller_frontend/src/components/TripInput.js
@@ -7,14 +7,21 @@ class TripInput extends Component {
         status: ''
     }
 
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 <form>
                     <label>Trip Name:</label>
-                    <input type="text" name="name" />
+                    <input onChange={this.handleOnChange} type="text" name="name" value={this.state.name} />
                     <label>Status:</label>
-                    <select>
+                    <select onChange={this.handleOnChange} value={this.state.status} name="status" >
+                        <option value="">Please Select an Option</option>
                         <option value="completed">Completed</option>
                         <option value="upcoming">Upcoming</option>
                         <option value="bucketlist">Bucket List</option>
