d81bfd9aa959f999794840c647b26b6da556bee9
rgrier104 <rgrier104@gmail.com>
2020-01-09 16:05:44 -1000

    connect trips container to redux store, add map to props and import fetch trips action to fetch when component mounts

--- traveller_frontend/src/containers/TripsContainer.js
+++ traveller_frontend/src/containers/TripsContainer.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import TripInput from '../components/TripInput';
 import Trips from '../components/Trips';
+import { fetchTrips } from '../actions/fetchTrips';
 
 class TripsContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchTrips()
+    }
+
     render() {
         return (
             <div>
                 <TripInput />
-                <Trips />
+                <Trips trips={this.props.trips} />
             </div>
         )
     }
 }
 
-export default TripsContainer;
+const mapStateToProps = state => {
+    return {
+        trips: state.trips
+    }
+}
+
+export default connect(mapStateToProps,{fetchTrips})(TripsContainer);
