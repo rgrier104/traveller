21e5ff2de4e9849efa37ac0abb2fd989fb48dc48
rgrier104 <rgrier104@gmail.com>
2020-01-09 16:29:47 -1000

    add new trip form and base state

--- traveller_frontend/src/components/TripInput.js
+++ traveller_frontend/src/components/TripInput.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class TripInput extends Component {
+
+    state = {
+        name: '',
+        status: ''
+    }
+
     render() {
         return (
             <div>
-                Trip Input
+                <form>
+                    <label>Trip Name:</label>
+                    <input type="text" name="name" />
+                    <label>Status:</label>
+                    <select>
+                        <option value="completed">Completed</option>
+                        <option value="upcoming">Upcoming</option>
+                        <option value="bucketlist">Bucket List</option>
+                    </select>
+                    <input type="submit" value="Create Trip" />
+                </form>
             </div>
         )
     }
