f680ba4fbc5a304740a197d82a5f5d02078b2618
rgrier104 <rgrier104@gmail.com>
2020-01-13 14:55:36 -1000

    add add trip case to reducer to update trips in state

--- traveller_frontend/src/reducers/tripReducer.js
+++ traveller_frontend/src/reducers/tripReducer.js
@@ -3,6 +3,8 @@ export default function tripReducer(state = { trips: [] }, action) {
     switch (action.type) {
         case 'FETCH_TRIPS':
             return { trips: action.payload }
+        case 'ADD_TRIP':
+            return { ...state, trips: [...state.trips, action.payload] }
         default:
             return state
     }
