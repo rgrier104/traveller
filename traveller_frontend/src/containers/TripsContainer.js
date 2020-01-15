f43b3720614534504c3e830842468416953ac710
rgrier104 <rgrier104@gmail.com>
2020-01-15 11:41:47 -1000

    add switch routes to render routes exclusively

--- traveller_frontend/src/containers/TripsContainer.js
+++ traveller_frontend/src/containers/TripsContainer.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import TripInput from '../components/TripInput';
 import Trips from '../components/Trips';
+import Trip from '../components/Trip';
 import { fetchTrips } from '../actions/fetchTrips';
 
 class TripsContainer extends Component {
@@ -14,9 +15,11 @@ class TripsContainer extends Component {
     render() {
         return (
             <div>
-                <Route path="/trips/new" component={TripInput} />
-                <Route path="/trips/:id" render={(routerProps) => <Trips {...routerProps} trips={this.props.trips} />} />
-                <Route exact path="/trips" render={(routerProps) => <Trips {...routerProps} trips={this.props.trips} />} />
+                <Switch>
+                    <Route path="/trips/new" component={TripInput} />
+                    <Route path="/trips/:id" render={(routerProps) => <Trip {...routerProps} trips={this.props.trips} />} />
+                    <Route path="/trips" render={(routerProps) => <Trips {...routerProps} trips={this.props.trips} />} />
+                </Switch>
             </div>
         )
     }
