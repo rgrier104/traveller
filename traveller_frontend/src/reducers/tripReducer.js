dcecaffc1058256db794af615fd6ca8093ced5e1
rgrier104 <rgrier104@gmail.com>
2020-01-15 17:09:57 -1000

    add case for deleting recommendation to reducer

--- traveller_frontend/src/reducers/tripReducer.js
+++ traveller_frontend/src/reducers/tripReducer.js
@@ -7,7 +7,10 @@ export default function tripReducer(state = { trips: [] }, action) {
             return { ...state, trips: [...state.trips, action.payload] }
         case 'ADD_RECOMMENDATION':
             let trips = state.trips.map(trip => trip.id === action.payload.id ? action.payload : trip)
-            return { ...state, trips: trips}
+            return { ...state, trips: trips }
+        case 'DELETE_RECOMMENDATION':
+            let tripsDelete = state.trips.map(trip => trip.id === action.payload.id ? action.payload : trip)
+            return { ...state, trips: tripsDelete }
         default:
             return state
     }
