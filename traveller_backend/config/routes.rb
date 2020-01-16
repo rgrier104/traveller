32d60be640d247083675fc1c4975b44abb128b96
rgrier104 <rgrier104@gmail.com>
2020-01-15 15:08:38 -1000

    nest routes so recommendations is nested inside trips

--- traveller_backend/config/routes.rb
+++ traveller_backend/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :recommendations
-      resources :trips
+      resources :trips do 
+        resources :recommendations
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
