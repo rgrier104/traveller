ba4cc45069ab7c2404af27ba951527cf70701bd3
rgrier104 <rgrier104@gmail.com>
2020-01-15 15:08:13 -1000

    add create controller method and strong params from recommendations controller

--- traveller_backend/app/controllers/api/v1/recommendations_controller.rb
+++ traveller_backend/app/controllers/api/v1/recommendations_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::RecommendationsController < ApplicationController
+
+    def create
+        @recommendation = Recommendation.new(recommendation_params)
+        if @recommendation.save
+            render json: @recommendation
+        else
+            render json: {error: 'Error creating recommendation'}
+        end
+    end
+
+    private
+
+    def recommendation_params
+        params.require(:recommendation).permit(:title, :description, :trip_id)
+    end
+
 end
