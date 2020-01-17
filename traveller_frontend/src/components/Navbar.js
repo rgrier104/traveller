a190ed344f585af6afdf2acbb8ffb9acc34ed5b8
rgrier104 <rgrier104@gmail.com>
2020-01-16 16:34:17 -1000

    add new routes to navbar

--- traveller_frontend/src/components/Navbar.js
+++ traveller_frontend/src/components/Navbar.js
@@ -5,12 +5,12 @@ class Navbar extends Component {
     render() {
         return (
             <div className="navbar">
-                <NavLink to="/trips" exact>
-                    Trips
-                </NavLink>
-                <NavLink to="/trips/new" exact>
-                    Create New Trip
-                </NavLink>
+                <NavLink to="/" exact>Home</NavLink>
+                <NavLink to="/trips" exact>All Trips</NavLink>
+                <NavLink to="/trips/completed" exact>Completed Trips</NavLink>
+                <NavLink to="/trips/upcoming" exact>Upcoming Trips</NavLink>
+                <NavLink to="/trips/bucketlist" exact>Bucket List Trips</NavLink>
+                <NavLink to="/trips/new" exact>Create New Trip</NavLink>
             </div>
         )
     }
